c33f93a9af40028fbef933505da7713be7e5684c
Supansa Tantulset <supansa.tantulset@gmail.com>
2024-07-15 15:46:09 +0700

    add api login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { ObjectProperty } from './../common/swagger/custom-response';
+import { Body, Controller, Post, HttpCode, HttpStatus, InternalServerErrorException, BadRequestException, NotFoundException, UseInterceptors } from '@nestjs/common';
+import { ApiExtraModels, ApiResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
+import { ISignInRequest, ISignInResponse } from 'src/common/interface/signIn.dto';
+import { ApiResponseWithType } from 'src/common/swagger/custom-response';
+import { AuthService } from './auth.service';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) { }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @ApiExtraModels(ISignInResponse)
+  @ApiResponse(ApiResponseWithType(ObjectProperty(getSchemaPath(ISignInResponse))))
+  login(@Body() loginDto: ISignInRequest): ISignInResponse {
+    return this.authService.login(loginDto.username);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ISignInResponse } from 'src/common/interface/signIn.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { userList } from 'src/common/constant/user.constant';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+import { jwtConstants } from './constant/jwt.constant';
+import { IUser } from 'src/common/interface/user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly jwtOptions: JwtSignOptions;
+  private readonly usernameList: IUser[] = userList;
+
+  constructor(
+    private jwtService: JwtService
+  ) {
+    this.usernameList = userList;
+    this.jwtOptions = {
+      secret: jwtConstants.secret,
+    };
+  }
+
+  login(username: string): ISignInResponse {
+    const userInfo = this.usernameList.find((u) => u.username === username);
+    if (!userInfo) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.generateToken(userInfo);
+  }
+
+  generateToken(userInfo: IUser): ISignInResponse {
+    const payload = { userId: userInfo.userId, username: userInfo.username };
+    let response = new ISignInResponse();
+    response.accessToken = this.jwtService.sign(payload, this.jwtOptions);
+    return response;
+  }
+
+  getUsername(userId: string): string {
+    return this.usernameList.find(u => u.userId === userId).username ?? '';
+  }
+}
