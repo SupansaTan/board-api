93575bcfc0a4a893cded43272d2a5b892b485d81
Supansa Tantulset <supansa.tantulset@gmail.com>
2024-07-15 15:50:37 +0700

    add comment service

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 import { CommentService } from './comment.service';
+import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PostService } from 'src/post/post.service';
 
 @Module({
-  providers: [CommentService]
+  providers: [
+    CommentService,
+    AuthService,
+    JwtService,
+    PostService,
+  ]
 })
-export class CommentModule {}
+export class CommentModule { }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { commentList } from 'src/common/constant/comment.constant';
+import { IComment } from 'src/common/interface/comment.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  private readonly commentList: IComment[] = commentList;
+
+  constructor(
+    private authService: AuthService
+  ) {
+    this.commentList = commentList;
+  }
+
+  getCommentList(postId: string) {
+    const filteredComment = this.commentList.filter(c => c.postId === postId);
+    const updatedComment = filteredComment.map((c) => ({
+      ...c,
+      postBy: this.authService.getUsername(c.postBy),
+    }));
+
+    const sortedCommenet = updatedComment.sort((prev, curr) =>
+      prev.createDate.getTime() - curr.createDate.getTime()
+    );
+
+    return sortedCommenet;
+  }
+
+  getTotalComment(postId: string): number {
+    return this.commentList.filter(c => c.postId === postId)?.length ?? 0;
+  }
+
+  addComment(postId: string, userId: string, comment: string): boolean {
+    let newComment = new IComment();
+    newComment.commentId = uuid();
+    newComment.comment = comment;
+    newComment.createDate = new Date();
+    newComment.postId = postId;
+    newComment.postBy = userId;
+
+    this.commentList.push(newComment);
+    return true;
+  }
+}
