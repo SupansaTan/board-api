06367429795ddb3fab56322d17958d4abb354e84
Supansa Tantulset <supansa.tantulset@gmail.com>
2024-07-16 07:23:42 +0700

    add unit test auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { ISignInRequest, ISignInResponse } from 'src/common/interface/signIn.dto';
+import { AuthService } from './auth.service';
+
+const mockSignInRequest: ISignInRequest = {
+  username: 'testuser',
+};
+
+const mockSignInResponse: ISignInResponse = {
+  accessToken: 'mockAccessToken',
+  username: mockSignInRequest.username
+};
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
+
+  const mockAuthService = {
+    login: jest.fn((username: string): ISignInResponse => mockSignInResponse),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return an access token', () => {
+      const result = authController.login(mockSignInRequest);
+      expect(result).toEqual(mockSignInResponse);
+      expect(authService.login).toHaveBeenCalledWith(mockSignInRequest.username);
+    });
+
+    it('should return a response with status 200', () => {
+      const response = authController.login(mockSignInRequest);
+      expect(response).toEqual(mockSignInResponse);
+    });
   });
 });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { ObjectProperty } from './../common/swagger/custom-response';
-import { Body, Controller, Post, HttpCode, HttpStatus, InternalServerErrorException, BadRequestException, NotFoundException, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { ApiExtraModels, ApiResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
-import { ISignInRequest, ISignInResponse } from 'src/common/interface/signIn.dto';
-import { ApiResponseWithType } from 'src/common/swagger/custom-response';
+import { ISignInRequest, ISignInResponse } from '../common/interface/signIn.dto';
+import { ApiResponseWithType } from '../common/swagger/custom-response';
 import { AuthService } from './auth.service';
 
 @ApiTags('Auth')
